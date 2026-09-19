495e1df07bc4a5a81d722059a8e04ba0bd1dceb8
machidashibu <machidashibu@gmail.com>
2026-09-20 00:17:59 +0900

    Modifies to wait to established server in test, because test is an unstable

--- internal/infra/server/echo_server_test.go
+++ internal/infra/server/echo_server_test.go
@@ -2,7 +2,9 @@ package server_test
 
 import (
 	"adbcon/internal/infra/server"
+	"net"
 	"testing"
+	"time"
 
 	"github.com/go-openapi/testify/v2/require"
 )
@@ -19,6 +21,25 @@ func (c stubConfig) ServerPort() string {
 	return c.port
 }
 
+// utility: wait server is established
+func waitListening(t *testing.T, address string) {
+	t.Helper()
+
+	deadline := time.Now().Add(3 * time.Second)
+
+	for time.Now().Before(deadline) {
+		conn, err := net.DialTimeout("tcp", address, 100*time.Millisecond)
+		if err == nil {
+			_ = conn.Close()
+			return
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	t.Fatalf("server did not start listening on %s", address)
+}
+
 func TestEchoServer(t *testing.T) {
 	// testing: server start
 	srv := server.NewEchoServer()
@@ -26,6 +47,9 @@ func TestEchoServer(t *testing.T) {
 	go func() {
 		require.NoError(t, srv.Start(stubConfig{port: "8080"}))
 	}()
+
+	// wait 1st server is established
+	waitListening(t, "127.0.0.1:8080")
 
 	// testing: confrict port
 	srv2nd := server.NewEchoServer()
